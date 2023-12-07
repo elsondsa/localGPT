77827dbb726c4c864936e4f3c377cdfc22d94c1d
Elson DSa <36267884+elsondsa@users.noreply.github.com>
2023-12-07 15:14:22 +0530

    simplified code

--- run_localGPT.py
+++ run_localGPT.py
@@ -1,6 +1,5 @@
 import os
 import logging
-import click
 import torch
 import utils
 import sys
@@ -161,121 +160,28 @@ def retrieval_qa_pipline(device_type, use_history, promptTemplate_type="llama"):
 
     return qa
 
+def main():
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s", level=logging.INFO
+    )
 
-# chose device typ to run on as well as to show source documents.
-@click.command()
-@click.option(
-    "--device_type",
-    default="cuda" if torch.cuda.is_available() else "cpu",
-    type=click.Choice(
-        [
-            "cpu",
-            "cuda",
-            "ipu",
-            "xpu",
-            "mkldnn",
-            "opengl",
-            "opencl",
-            "ideep",
-            "hip",
-            "ve",
-            "fpga",
-            "ort",
-            "xla",
-            "lazy",
-            "vulkan",
-            "mps",
-            "meta",
-            "hpu",
-            "mtia",
-        ],
-    ),
-    help="Device to run on. (Default is cuda)",
-)
-@click.option(
-    "--show_sources",
-    "-s",
-    is_flag=True,
-    help="Show sources along with answers (Default is False)",
-)
-@click.option(
-    "--use_history",
-    "-h",
-    is_flag=True,
-    help="Use history (Default is False)",
-)
-@click.option(
-    "--model_type",
-    default="llama",
-    type=click.Choice(
-        ["llama", "mistral", "non_llama"],
-    ),
-    help="model type, llama, mistral or non_llama",
-)
-@click.option(
-    "--save_qa",
-    is_flag=True,
-    help="whether to save Q&A pairs to a CSV file (Default is False)",
-)
-@click.command()
-@click.option("--device_type", default="cuda" if torch.cuda.is_available() else "cpu", type=click.Choice([...]), help="Device to run on. (Default is cuda)")
-# ... [other click options]
-@click.option("--query", prompt="Enter a query", help="Query to process")
-def main(device_type, show_sources, use_history, model_type, save_qa, query):
-    """
-    Implements the main information retrieval task for a localGPT.
+    device_type = "cuda" if torch.cuda.is_available() else "cpu"
+    use_history = False  # Set your default value
+    model_type = "llama"  # Set your default value
 
-    This function sets up the QA system by loading the necessary embeddings, vectorstore, and LLM model.
-    It then enters an interactive loop where the user can input queries and receive answers. Optionally,
-    the source documents used to derive the answers can also be displayed.
-
-    Parameters:
-    - device_type (str): Specifies the type of device where the model will run, e.g., 'cpu', 'mps', 'cuda', etc.
-    - show_sources (bool): Flag to determine whether to display the source documents used for answering.
-    - use_history (bool): Flag to determine whether to use chat history or not.
-
-    Notes:
-    - Logging information includes the device type, whether source documents are displayed, and the use of history.
-    - If the models directory does not exist, it creates a new one to store models.
-    - The user can exit the interactive loop by entering "exit".
-    - The source documents are displayed if the show_sources flag is set to True.
-
-    """
-
-    logging.info(f"Running on: {device_type}")
-    logging.info(f"Display Source Documents set to: {show_sources}")
-    logging.info(f"Use history set to: {use_history}")
-
-    # check if models directory do not exist, create a new one and store models here.
     if not os.path.exists(MODELS_PATH):
         os.mkdir(MODELS_PATH)
 
     qa = retrieval_qa_pipline(device_type, use_history, promptTemplate_type=model_type)
-    # Get the answer from the chain
+    
+    query = sys.argv[1] if len(sys.argv) > 1 else "Default Query"
     res = qa(query)
     answer, docs = res["result"], res["source_documents"]
 
-    # Print the result
     print("\n\n> Question:")
     print(query)
     print("\n> Answer:")
     print(answer)
 
-    if show_sources:  # this is a flag that you can set to disable showing answers.
-        # # Print the relevant sources used for the answer
-        print("----------------------------------SOURCE DOCUMENTS---------------------------")
-        for document in docs:
-            print("\n> " + document.metadata["source"] + ":")
-            print(document.page_content)
-        print("----------------------------------SOURCE DOCUMENTS---------------------------")
-    
-    # Log the Q&A to CSV only if save_qa is True
-    if save_qa:
-        utils.log_to_csv(query, answer)
-
-
 if __name__ == "__main__":
-    logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s", level=logging.INFO
-    )
     main()
